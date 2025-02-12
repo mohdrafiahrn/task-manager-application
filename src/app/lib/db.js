e3e474633834f1cc218639045f6b60e9e365dc2e
Mohd Rafi Hussain rainy <mohd.rafiahrn@gmail.com>
2025-02-12 22:04:46 +0530

    db.js

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,17 +1,15 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI; //Local MongoDB Compass
+const MONGODB_URI = process.env.MONGODB_URI; // MongoDB URI from environment variable
 
 const connectDB = async () => {
   if (mongoose.connections[0].readyState) {
-    return;
+    return; // If already connected, no need to reconnect
   }
 
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // Updated mongoose connection logic without deprecated options
+    await mongoose.connect(MONGODB_URI); // Use MONGODB_URI directly
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
